af637e25a250f63d76eb8db552ca2cf826403f9e
Mario Alameda <mralameda16@gmail.com>
2022-05-02 16:57:29 -0700

    delete

---
+++ src/Items.js
@@ -0,0 +1,16 @@
+
+let saludo="hola"
+
+function Items(){
+    return(
+        <section>
+            <div Class="Container-Card">
+                <div Class="titleProduct">
+                    `${saludo}`
+                </div> 
+            </div>
+        </section>
+    )
+}
+
+export { Items }
